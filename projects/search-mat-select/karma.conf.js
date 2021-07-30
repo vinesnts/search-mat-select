65f6a14f0b3a9f96361fd91c02ee6d5d5bba93b4
vinesnts <v.santos0406@gmail.com>
2021-07-30 18:39:24 -0300

    added travis configuration

--- projects/search-mat-select/karma.conf.js
+++ projects/search-mat-select/karma.conf.js
@@ -39,6 +39,13 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    }
   });
+  
 };
